2f088ba98b105891ae88ef1fb9520825a436d9bc
LaveeshkaMahadea <laveeshka94@gmail.com>
2022-09-24 23:33:04 +1000

    manually increment and decrement timer

--- js/challenge.js
+++ js/challenge.js
@@ -4,12 +4,37 @@
 //do work on page load
 document.addEventListener("DOMContentLoaded", initialise)
 
+//declare variables here
+const counter = document.querySelector('#counter')
+const minusBtn = document.querySelector('#minus');
+const plusBtn = document.querySelector('#plus');
+let compte = 0;
+const myInterval = () => setInterval(startTimer, 1000);
+
 //As a user, I can see the timer increment every second once the page has loaded
+//As a user, I can manually increment and decrement the counter using the plus and minus buttons
 function initialise(){
-    const counter = document.querySelector('#counter');
-    console.log(counter);
-    setInterval(() => {
-        counter.innerHTML = parseInt(counter.innerHTML);
-        counter.innerHTML++;
-    }, 1000)
+
+    myInterval();
+
+    plusBtn.addEventListener("click", () => {
+        console.log("clickity clackity thang");
+        compte++;
+        counter.innerHTML = compte;
+    });
+
+    minusBtn.addEventListener("click", () => {
+        console.log("clickity clackity thang");
+        compte--;
+        counter.innerHTML = compte;
+    })
 }
+
+function startTimer(){
+    compte++;
+    counter.innerHTML = compte;
+}
+
+// function stopTimer(){
+//     clearInterval(myInterval);
+// }
