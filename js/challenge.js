49abb3cc50abff95b62c2c79e525bee66ce3912d
LaveeshkaMahadea <laveeshka94@gmail.com>
2022-09-25 00:41:06 +1000

    pause the counter

--- js/challenge.js
+++ js/challenge.js
@@ -9,14 +9,18 @@ const counter = document.querySelector('#counter')
 const minusBtn = document.querySelector('#minus');
 const plusBtn = document.querySelector('#plus');
 const coeur = document.querySelector('#heart');
+const pauseBtn = document.querySelector('#pause');
+const submitBtn = document.querySelector('#submit');
 const likes = document.querySelector('.likes');
 let compte = 0;
 let likesObj = {}; //will store the number and count of likes
+let isPaused = false;
 const myInterval = () => setInterval(startTimer, 1000);
 
 //As a user, I can see the timer increment every second once the page has loaded
 //As a user, I can manually increment and decrement the counter using the plus and minus buttons
 //As a user, I can "like" an individual number of the counter. I should see the count of the number of "likes" associated with that number displayed.
+//As a user, I can pause the counter which should pause the counter, disable all buttons except the pause button and switch the label on the button from "pause" to "resume"
 function initialise(){
 
     myInterval();
@@ -50,11 +54,36 @@ function initialise(){
         }
 
     })
+
+    pauseBtn.addEventListener("click", () => {
+        if (pauseBtn.textContent !== "resume") {
+            console.log("clickity clackity thang");
+            minusBtn.setAttribute('disabled', '');
+            plusBtn.setAttribute('disabled', '');
+            coeur.setAttribute('disabled', '');
+            submitBtn.setAttribute('disabled', '');
+            pauseBtn.textContent = "resume";
+            isPaused = true;
+            counter.innerHTML = compte;
+        }
+        else {
+            pauseBtn.textContent = "pause";
+            minusBtn.removeAttribute('disabled');
+            plusBtn.removeAttribute('disabled');
+            coeur.removeAttribute('disabled');
+            submitBtn.removeAttribute('disabled');
+            isPaused = false;
+        }
+        
+    });
 }
 
 function startTimer(){
-    compte++;
-    counter.innerHTML = compte;
+    if (!isPaused){
+        compte++;
+        counter.innerHTML = compte;
+    }
+    
 }
 
 // function stopTimer(){
