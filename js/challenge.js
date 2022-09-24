0fe41f9e4439e9ea949783ea084f46707600aacc
LaveeshkaMahadea <laveeshka94@gmail.com>
2022-09-25 00:05:59 +1000

    like an individual number of the counter

--- js/challenge.js
+++ js/challenge.js
@@ -8,11 +8,15 @@ document.addEventListener("DOMContentLoaded", initialise)
 const counter = document.querySelector('#counter')
 const minusBtn = document.querySelector('#minus');
 const plusBtn = document.querySelector('#plus');
+const coeur = document.querySelector('#heart');
+const likes = document.querySelector('.likes');
 let compte = 0;
+let likesObj = {}; //will store the number and count of likes
 const myInterval = () => setInterval(startTimer, 1000);
 
 //As a user, I can see the timer increment every second once the page has loaded
 //As a user, I can manually increment and decrement the counter using the plus and minus buttons
+//As a user, I can "like" an individual number of the counter. I should see the count of the number of "likes" associated with that number displayed.
 function initialise(){
 
     myInterval();
@@ -27,6 +31,24 @@ function initialise(){
         console.log("clickity clackity thang");
         compte--;
         counter.innerHTML = compte;
+    })
+
+    coeur.addEventListener("click", () => {
+        console.log("clickity clackity thang");
+        console.log(likesObj);
+        if (likesObj[compte] && document.getElementById(`${compte}`)) {
+            const existingLi = document.getElementById(`${compte}`);
+            likesObj[compte]++;
+            existingLi.innerHTML = `${compte} has been liked ${likesObj[compte]} times`;
+        }
+        else {
+            likesObj[compte] = 1;
+            const li = document.createElement("li");
+            li.setAttribute('id', `${compte}`);
+            li.innerHTML = `${compte} has been liked ${likesObj[compte]} time`;
+            likes.appendChild(li);
+        }
+
     })
 }
 
